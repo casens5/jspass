9589e396e14c54d43a461a1a02146a582b2a319c
rudy ordoyne <rudolfordoyne@protonmail.com>
2020-09-18 20:44:03 -0400

    fix everything

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jspass
 browser-based passphrase generator, try it at https://ludant.github.io/jspass/, probably
+
+when testing, use `python3 -m http.server` to fix CORS errors, and use an incognito session to have CSS updates work properly
--- script.js
+++ script.js
@@ -1,89 +1,102 @@
+'use strict'
+
 function $(id) {
-  return document.getElementById(id);
+	return document.getElementById(id);
 }
 
 // js is a second-rate language and we all know it
 function capitalize(word) {
-  return word.substring(0, 1).toUpperCase() + word.substring(1);
+	return word.substring(0, 1).toUpperCase() + word.substring(1);
+}
+
+// function dictAvg(dictionary, domElement) {
+//	 const reducer = (a, b) => {
+//		 return a + b.length;
+//	 };
+//	 let number = (dictionary.reduce(reducer, 0) / dictionary.length).toPrecision(
+//		 3
+//	 );
+//	 domElement.textContent = number + ' characters';
+// }
+
+function newPhrase(dictionary, length = 4, separator = ' ', caps = 'false') {
+	if (length < 1) {
+		length = 4;
+	}
+	if (length > 99) {
+		length = 99;
+	}
+
+	let camel = false;
+	// don't judge me
+	if (caps === 'camel') {
+		caps = 'true';
+		camel = true;
+	}
+
+	const phrase = [];
+	let word;
+	for (let i = 0; i < length; i += 1) {
+		word = dictionary[Math.floor(Math.random() * dictionary.length)];
+		if (caps === 'true') {
+			word = capitalize(word);
+		}
+		phrase.push(word);
+	}
+	if (camel) {
+		phrase[0] = phrase[0].toLowerCase();
+	}
+	$('phraseDisplay').textContent = phrase.join(separator);
 }
 
 const multilingualURL = 'dictionaries/multilingual.json';
 const diceURL = 'dictionaries/diceware.json';
 const effURL = 'dictionaries/eff.json';
 
-// const multilingualURL = 'https://raw.githubusercontent.com/ludant/jspass/master/dictionaries/multilingual.json';
-// const diceURL = 'https://raw.githubusercontent.com/ludant/jspass/master/dictionaries/diceware.json';
-// const effURL = 'https://raw.githubusercontent.com/ludant/jspass/master/dictionaries/eff.json';
+//	const multilingualWebURL = 'https://raw.githubusercontent.com/ludant/jspass/master/dictionaries/multilingual.json';
+//	const diceWebURL = 'https://raw.githubusercontent.com/ludant/jspass/master/dictionaries/diceware.json';
+//	const effWebURL = 'https://raw.githubusercontent.com/ludant/jspass/master/dictionaries/eff.json';
+
 const dictionaries = {};
 
 $('instructionsLink').addEventListener('click', () => {
-  $('info').classList.toggle('hidden');
+	$('info').classList.toggle('hidden');
 });
 
 $('generateButton').addEventListener('click', () => {
-  newPhrase(
-    dictionaries[$('dictionarySelect').value],
-    $('phraseLength').value,
-    $('wordSeparator').value,
-    $('capitalsSelect').value,
-  );
+	newPhrase(
+		dictionaries[$('dictionarySelect').value],
+		$('phraseLength').value,
+		$('wordSeparator').value,
+		$('capitalsSelect').value,
+	);
 });
 
-// function dictAvg(dictionary, domElement) {
-//   const reducer = (a, b) => {
-//     return a + b.length;
-//   };
-//   let number = (dictionary.reduce(reducer, 0) / dictionary.length).toPrecision(
-//     3
-//   );
-//   domElement.textContent = number + ' characters';
-// }
-
-function newPhrase(dictionary, length = 4, separator = ' ', caps = 'false') {
-  if (length < 1) {
-    length = 4;
-  }
-  if (length > 99) {
-    length = 99;
-  }
-
-  let camel = false;
-  // don't judge me
-  if (caps === 'camel') {
-    caps = 'true';
-    camel = true;
-  }
-
-  const phrase = [];
-  let word;
-  for (let i = 0; i < length; i += 1) {
-    word = dictionary[Math.floor(Math.random() * dictionary.length)];
-    if (caps === 'true') {
-      word = capitalize(word);
-    }
-    phrase.push(word);
-  }
-  if (camel) {
-    phrase[0] = phrase[0].toLowerCase();
-  }
-  $('phraseDisplay').value = phrase.join(separator);
+async function loadDict(url, dictName, generate = false) {
+	const response = await fetch(url);
+	if (response.ok) {
+		dictionaries[dictName] = await response.json();
+		console.log(generate)
+		if (generate) {
+			newPhrase(dictionaries[dictName], 4, ' ', 'false');
+		};
+	} else {
+		console.log('fuck, ' + response.status);
+	}
 }
 
-function loadDict(url, dictionaryName, generate = false) {
-  const xobj = new XMLHttpRequest();
-  xobj.overrideMimeType('application/json');
-  xobj.open('GET', url, true);
-  xobj.responseType = 'json';
-  xobj.onreadystatechange = () => {
-		console.log(xobj);
-    if (xobj.readyState === 4 && xobj.status === '200') {
-      dictionaries[dictionaryName] = xobj.response;
-      if (generate) {
-        newPhrase(dictionaries[dictionaryName], 4, ' ', 'false');
-      }
-    }
-  };
-  xobj.send(null);
+async function beeb(url, name, generate = false) {
+  fetch(url)
+	.then(response => response.json())
+	.then(data => {
+		dictionaries[name] = data;
+		if (generate) {
+			newPhrase(dictionaries[name], 4, ' ', 'false');
+		};
+	})
+	.catch((error) => {
+		console.error(error);
+	});
 }
 
 loadDict(diceURL, 'diceware');
